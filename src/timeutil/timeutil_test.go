783354e07ed9078fe7f21d940b6492abc0d44721
Liu, Jeremy <jeremy.liu@elekta.com>
2017-02-13 09:13:42 +0800

    Use testify to add unit tests for timeutil

--- src/timeutil/timeutil_test.go
+++ src/timeutil/timeutil_test.go
@@ -3,19 +3,23 @@ package timeutil_test
 import ("testing"
 	"timeutil"
 	"time"
-
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseDate(t *testing.T)  {
 	dateString := "2017-01-02"
-	expectedMonth := time.January
 
 	result, err := timeutil.ParseDate(dateString)
 
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 2017, result.Year())
+	assert.Equal(t, time.January, result.Month())
+	assert.Equal(t, 2, result.Day())
+}
 
-	if err != nil {
-		t.Error("Failed")
-	}else if result.Month() != expectedMonth{
-		t.Error("Not expected month")
-	}
+func TestToString(t *testing.T) {
+	date := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	resultString := timeutil.ToString(date)
+
+	assert.Equal(t, "2009-11-10", resultString)
 }
